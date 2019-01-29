6be82efd1977553d45f1d3c04f291685ca1f57d7
DAVID BENGTSSON <dabg15@student.bth.se>
2019-01-29 13:21:23 +0100

    Created a class Character which is supposed to be foundation for every character class in the game.

---
+++ LillaSpelprojektet/character.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <GLM/vec4.hpp>
+
+// Forward declarations
+class Model;
+
+// This class is used as a base of each character in the game. 
+// Example:
+//    class Player : Character
+//    class FireGuy : Character
+
+class Character
+{
+public:
+	Character();
+	~Character();
+
+	virtual void Render(); // Update this when more other things are implemented
+
+	void SetPosition(glm_vec4 const pos);
+	void SetHealth(int const health);
+	void SetPower(int const power);
+	void SetModel(); // Update ths when model reader system is working
+
+	glm_vec4 GetPosition();
+	int GetHealth();
+	int GetPower();
+	Model* GetModel();
+private:
+	glm_vec4 mPosition_;
+	int mHealth_;
+	int mPower_ = 1;
+	
+	Model* mModel_;
+};
