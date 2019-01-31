d06e9d7ac203b8e43a5bf1fe175c561d20b0e668
SEBASTIAN VUSAK <sevu15@student.bth.se>
2019-01-31 09:33:07 +0100

    "cUrLy bRaCkEtS nEeDs tO bE On tHe sAmE rOw As a fUnCtiOn"

--- LillaSpelprojektet/light_handler.cpp
+++ LillaSpelprojektet/light_handler.cpp
@@ -1,7 +1,6 @@
 #include "light_handler.h"
 
-LightHandler::LightHandler()
-{
+LightHandler::LightHandler() {
 	//Scrap values
 	position_ = glm::vec3(0.0f, 10.0f, 0.0f);
 	brightness_ = glm::vec3(0.7f, 0.7f, 0.7f);
@@ -11,8 +10,7 @@ LightHandler::LightHandler()
 	
 }
 
-void LightHandler::LightDefault(glm::vec3 pos, glm::vec3 brightness, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular)
-{
+void LightHandler::LightDefault(glm::vec3 pos, glm::vec3 brightness, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular) {
 	position_ = pos;
 	brightness_ = brightness;
 	ambient_light_ = ambient;
@@ -20,56 +18,46 @@ void LightHandler::LightDefault(glm::vec3 pos, glm::vec3 brightness, glm::vec3 a
 	specular_light_ = specular;
 }
 
-LightHandler::~LightHandler()
-{
+LightHandler::~LightHandler() {
+
 }
 
-void LightHandler::SetPos(glm::vec3 pos)
-{
+void LightHandler::SetPos(glm::vec3 pos) {
 	position_ = pos;
 }
 
-glm::vec3 LightHandler::GetPos() const
-{
+glm::vec3 LightHandler::GetPos() const {
 	return position_;
 }
 
-void LightHandler::SetBrightness(glm::vec3 brightness)
-{
+void LightHandler::SetBrightness(glm::vec3 brightness) {
 	brightness_ = brightness;
 }
 
-glm::vec3 LightHandler::GetBrightness() const
-{
+glm::vec3 LightHandler::GetBrightness() const {
 	return brightness_;
 }
 
-void LightHandler::SetAmbientLight(glm::vec3 ambient)
-{
+void LightHandler::SetAmbientLight(glm::vec3 ambient) {
 	ambient_light_ = ambient;
 }
 
-glm::vec3 LightHandler::GetAmbientLight() const
-{
+glm::vec3 LightHandler::GetAmbientLight() const {
 	return ambient_light_;
 }
 
-void LightHandler::SetDiffuseConstant(glm::vec3 diffuse)
-{
+void LightHandler::SetDiffuseConstant(glm::vec3 diffuse) {
 	diffuse_constant_ = diffuse;
 }
 
-glm::vec3 LightHandler::GetDiffuseConstant() const
-{
+glm::vec3 LightHandler::GetDiffuseConstant() const {
 	return diffuse_constant_;
 }
 
-void LightHandler::SetSpecularLight(glm::vec3 specular)
-{
+void LightHandler::SetSpecularLight(glm::vec3 specular) {
 	specular_light_ = specular;
 }
 
-glm::vec3 LightHandler::GetSpecularLight() const
-{
+glm::vec3 LightHandler::GetSpecularLight() const {
 	return specular_light_;
 }
