17e73f2c8606ce7c1abd5062aafbcfd1e06c7af4
SEBASTIAN VUSAK <sevu15@student.bth.se>
2019-02-01 15:56:23 +0100

    changed in Lighting Pass so that the output is a screen texture rather than a "fragment color"

--- LillaSpelprojektet/glsl/lightingpass/lighting_fs.glsl
+++ LillaSpelprojektet/glsl/lightingpass/lighting_fs.glsl
@@ -1,10 +1,6 @@
 #version 440
 
-//	Will be used later instead of frag_color
-//	in order to apply post-processing effects
-//layout(location = 0) out vec3 screenTexture;
-
-out vec4 fragment_color;
+layout(location = 0) out vec3 screen_texture;
 
 in vec2 TexCoords;
 
@@ -14,8 +10,8 @@ uniform sampler2D g_albedo_spec;
 
 
 struct Light {
-	vec3 position_;
-	vec3 color_;
+	vec3 position;
+	vec3 color;
 };
 const int nr_of_lights = 1;
 uniform Light lights[nr_of_lights];
@@ -35,10 +31,10 @@ void main() {
 	vec3 lighting = albedo * 0.1; // hard-coded ambient component
 	vec3 view_dir = normalize(viewPos - fragment_position);
 	for (int i = 0; i < nr_of_lights; i++) {
-		vec3 light_direction = normalize(lights[i].position_ - fragment_position);
-		vec3 diffuse = max(dot(normal, light_direction), 0.0) * albedo * lights[i].color_;
+		vec3 light_direction = normalize(lights[i].position - fragment_position);
+		vec3 diffuse = max(dot(normal, light_direction), 0.0) * albedo * lights[i].color;
 		lighting += diffuse;
 	}
 
-	fragment_color = vec4(lighting, 1.0);
+	screen_texture = lighting;
 }
