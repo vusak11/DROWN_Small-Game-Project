9e687ef24aae0babddb9596fe7135a7f2851cfe9
JAKOB MÅNSSON <jamd15@student.bth.se>
2019-02-04 16:19:45 +0100

    Matrices used for the world matrix added to ObjectClass Added some functions to alter aforementioned matrices

--- LillaSpelprojektet/Object/object_class.cpp
+++ LillaSpelprojektet/Object/object_class.cpp
@@ -2,36 +2,81 @@
 
 //Private--------------------------------------------------
 
-
+void ObjectClass::CalculateWorldMatrix() {
+	//Add all matrices together into a world matrix.
+	//The order is important here.
+	//First we scale, then we rotate and finally we translate
+	this->world_matrix_ = this->translation_matrix_ * this->rotation_matrix_ * this->scaling_matrix_;
+}
 
 
 //Public---------------------------------------------------
 
 ObjectClass::ObjectClass() {
+	this->world_matrix_up_to_date_ = false;
 }
 
 ObjectClass::~ObjectClass() {
 }
  
-/*
-void ObjectClass::SetPosition(glm::vec4 const pos) {
-	position_.x = pos.x;
-	position_.y = pos.y;
-	position_.z = pos.z;
-	position_.w = pos.w;
+
+void ObjectClass::SetPosition(float in_x, float in_y, float in_z = 0.0f) {
+	position_.x = in_x;
+	position_.y = in_y;
+	position_.z = in_z;
+
+	//Translate an identity matrix to position_ and set it as the model's translation matrix
+	this->translation_matrix_ = glm::translate(glm::mat4(), position_);
+
+	//World matrix is now out of date
+	this->world_matrix_up_to_date_ = false;
 }
 
+void ObjectClass::SetScale(float in_s) {
+	scale_ = glm::vec3(in_s, in_s, in_s);
+
+	//Scale an identity matrix by scale_
+	this->scaling_matrix_ = glm::scale(glm::mat4(), this->scale_);
+
+	//World matrix is now out of date
+	this->world_matrix_up_to_date_ = false;
+}
+
+void ObjectClass::SetScale(float in_x, float in_y, float in_z) {
+	scale_ = glm::vec3(in_x, in_y, in_z);
+
+	//Scale an identity matrix by scale_
+	this->scaling_matrix_ = glm::scale(glm::mat4(), this->scale_);
+
+	//World matrix is now out of date
+	this->world_matrix_up_to_date_ = false;
+}
+
+void ObjectClass::SetRotation(float in_x, float in_y, float in_z) {
+	this->rotation_around_x_ = in_x;
+	this->rotation_around_y_ = in_y;
+	this->rotation_around_z_ = in_z;
+
+	//Create three matrices for rotating around x, y and z
+	glm::mat4 rotation_matrix_x = glm::rotate(this->rotation_around_x_, glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 rotation_matrix_y = glm::rotate(this->rotation_around_y_, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 rotation_matrix_z = glm::rotate(this->rotation_around_z_, glm::vec3(0.0f, 0.0f, 1.0f));
+
+	//Add the rotations together
+	this->rotation_matrix_ = rotation_matrix_x * rotation_matrix_y * rotation_around_z_;
+
+	//World matrix is now out of date
+	this->world_matrix_up_to_date_ = false;
+}
+
+
+
+/*
 void ObjectClass::SetVelocity(glm::vec4 const v) {
 	velocity_ = v;
 }
 
-void ObjectClass::SetRotation(glm::vec4 const r) {
-	rotation_ = r;
-}
 
-void ObjectClass::SetScale(glm::vec4 const s) {
-	scale_ = s;
-}
 */
 
 glm::vec3 ObjectClass::GetPosition() const {
@@ -39,8 +84,15 @@ glm::vec3 ObjectClass::GetPosition() const {
 }
 
 glm::vec4 ObjectClass::GetVelocityVector() const {
-	//Calculates a vec4 describing the directional velocity
+	//Calculates a vec4 describing the directional velocity and returns it
 	return glm::vec4(this->velocity_ * glm::normalize(this->move_direction_), 0.0);
+}
+
+glm::mat4 ObjectClass::GetWorldMatrix() {
+	//If the world matrix is not up to date call the function calculating it
+	if (!this->world_matrix_up_to_date_) { this->CalculateWorldMatrix(); }
+	
+	return this->world_matrix_;
 }
 
 /*
--- LillaSpelprojektet/Object/object_class.h
+++ LillaSpelprojektet/Object/object_class.h
@@ -3,13 +3,11 @@
 
 #include <GLM/vec4.hpp>
 #include <GLM/mat4x4.hpp>
-//#include "model_class"
-
-// Forward declarations
-class Model;
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtx/transform.hpp>
 
 // This class is used as a base for all the objects in the game. This goes for any
-// objects and characters.
+// objects or characters.
 // Example:
 //    class Character : ObjectClass
 
@@ -18,21 +16,33 @@ class ObjectClass {
 private:
 	//Variables-----------------------------------------------
 	glm::vec3 position_;
-	glm::vec3 rotation_;		//Vector with	rotation around x-axis on x,
+	//glm::vec3 rotation_;		//Vector with	rotation around x-axis on x,
 								//				rotation around y-axis on y
 								//				and rotation around z-axis on z
+								//Rotation represented in degrees
+
+	float rotation_around_x_;			//The rotation of the object in degrees 
+	float rotation_around_y_;			//around the x, y and z-axises
+	float rotation_around_z_;			//
+
 	glm::vec3 scale_;			//Vector with	x scaling on x,
 								//				y scaling on y
 								//				and z scaling on z
 
-	bool up_to_date_;				//NTS: A variable that should be set to false every time position, rotation or scale is updated
+
+
+	glm::mat4 translation_matrix_;
+	glm::mat4 scaling_matrix_;
+	glm::mat4 rotation_matrix_;
+
+	bool world_matrix_up_to_date_;		//NTS:	Variable should be set to false anytime we change one of the other matrices
 	glm::mat4 world_matrix_;
 
 	float velocity_;
 	glm::vec3 move_direction_;
 
 	//Functions----------------------------------------------
-	void CalculateWorldMatrix();	//NTS: This function should be called before returning the world matrix if up_to_date_ is false
+	void CalculateWorldMatrix();	//Function calculates world matrix from scratch
 
 	//Model* model_; // The model class will contain a TextureClass 
 	//HitBot hitbox_;
@@ -41,15 +51,24 @@ public:
 	ObjectClass();
 	~ObjectClass();
 
-	void SetPosition(glm::vec4 const pos);
+	void SetPosition(float in_x, float in_y, float in_z = 0.0f);
+
+	void SetScale(float in_s);
+	void SetScale(float in_x, float in_y, float in_z);
+
+	void SetRotation(float in_x, float in_y, float in_z);
+	
+	
 	void SetVelocity(glm::vec4 const v);
-	void SetRotation(glm::vec4 const r);
-	void SetScale(glm::vec4 const s);
+	
+	
 
 	glm::vec3 GetPosition() const;				//Returns the object's x, y and z coordinates
 	glm::vec4 GetVelocityVector() const;		//Returns a vec4 with the object's velocity vector (direction of movement times velocity)
 
-	glm::mat4 GetWorldMatrix();					//NTS: Function not 'const' declared as it should update the matrix if it isn't up to date
+	glm::mat4 GetWorldMatrix();					//NTS: Should check if world matrix is up to date before returning, and update it if it isn't
+
+	void UpdatePosition(float in_deltatime);	//Moves object along it's velocity vector in accorance with time elapsed
 
 	//glm::vec4 GetRotation() const;
 	//glm::vec4 GetScale() const;
