330090efa5c6bc481451d28825abe7370679c5a3
SEBASTIAN VUSAK <sevu15@student.bth.se>
2019-02-15 13:50:26 +0100

    Removed redundant line of code

--- LillaSpelprojektet/gui.cpp
+++ LillaSpelprojektet/gui.cpp
@@ -38,12 +38,12 @@ void GUI::RenderGUI(ShaderHandler* shader_program) {
 
 	// Update content of VBO memory
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_);
-	//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(bar_vertices), bar_vertices);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(bar_vertices), &bar_vertices, GL_STATIC_DRAW); // Be sure to use glBufferSubData and not glBufferData
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	// Render bar
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
