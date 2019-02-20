37818ba65c3096f65f89eab412e001b90ca99fa1
DAVID BENGTSSON <dabg15@student.bth.se>
2019-02-20 15:46:56 +0100

    pushing into development - dc

--- LillaSpelprojektet/Globals.h
+++ LillaSpelprojektet/Globals.h
@@ -29,8 +29,8 @@
 #define PLAYER_JUMP_VELOCITY 300
 
 //Map------------------------------------------------------
-#define GRID_COLUMN 4
-#define GRID_ROW 4
+#define GRID_COLUMN 16
+#define GRID_ROW 16
 //#define MAP_SCALE 1						//1 -> each vertex have 1 position unit in between
 #define MAP_DEPTH 0.2					//The depth of the heightmap
 
